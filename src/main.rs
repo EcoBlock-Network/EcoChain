0471abc9531790d79bcf3af3b2b94983ad77612f
Winrhy <malo@arkada.fr>
2024-11-25 01:30:53 +0100

    feat: :sparkles: Test basic transaction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[derive(Debug)]
+struct Transaction {
+    id: String,
+    data: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let tx = Transaction {
+        id: "T1".to_string(),
+        data: "Donnée de test".to_string(),
+    };
+
+    println!("Transaction créée {:?}", tx);
 }
