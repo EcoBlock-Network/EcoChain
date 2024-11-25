48f72c2208a44ec043dc7769c4dabffeee26d940
Winrhy <malo@arkada.fr>
2024-11-25 01:42:47 +0100

    feat: :triangular_flag_on_post: add approvers

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::{collections::HashMap, hash::Hash};
+use std::{collections::HashMap, hash::Hash, vec};
 
 #[derive(Debug)]
 struct Transaction {
     id: String,
     data: String,
+    approvers: Vec<String>,
 }
 
 #[derive(Debug)]
@@ -20,10 +21,11 @@ impl DAG {
     }
 
     //add a transaction to the DAG
-    fn add_transaction(&mut self, id: String, data: String) {
+    fn add_transaction(&mut self, id: String, data: String, approvers: Vec<String>) {
         let transaction = Transaction {
             id: id.clone(),
             data: data,
+            approvers,
         };
         self.transations.insert(id, transaction);
     }
@@ -31,15 +33,45 @@ impl DAG {
     //get a transaction from the DAG
     fn display(&self) {
         for (id, transaction) in &self.transations {
-            println!("Transaction id: {}, data: {}", id, transaction.data);
+            println!("Transaction id: {}, data: {}, Approvers {}", id, transaction.data, transaction.approvers.join(", "));
+        }
+    }
+
+    //validate the DAG
+    fn validate_transaction(&self, id: String) -> bool {
+        let transaction = self.transations.get(&id);
+        match transaction {
+            Some(transaction) => {
+                for approver in &transaction.approvers {
+                    if !self.transations.contains_key(approver) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            None => return false,
         }
     }
 }
 
 fn main() {
+    // Créer un DAG
     let mut dag = DAG::new();
-    dag.add_transaction("1".to_string(), "Transaction 1".to_string());
-    dag.add_transaction("2".to_string(), "Transaction 2".to_string());
-    dag.add_transaction("3".to_string(), "Transaction 3".to_string());
-    dag.display();
+
+    // Ajouter des transactions avec des liens
+    dag.add_transaction("T1".to_string(), "Transaction 1".to_string(), vec![]);
+    dag.add_transaction(
+        "T2".to_string(),
+        "Transaction 2".to_string(),
+        vec!["T1".to_string()],
+    );
+    dag.add_transaction(
+        "T3".to_string(),
+        "Transaction 3".to_string(),
+        vec!["T1".to_string(), "T2".to_string()],
+    );
+
+    // Valider des transactions
+    println!("T3 est valide ? {}", dag.validate_transaction(&"T3".to_string())); // true
+    println!("T4 est valide ? {}", dag.validate_transaction(&"T4".to_string())); // false
 }
