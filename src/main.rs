5e74c792dd9feb868d6ff4ac80b4f96f9ce3f819
HenryMalo <winrhy@gmail.com>
2024-11-26 00:29:52 +0100

    feat: :sparkles: generate key pair

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,32 @@ mod dag;
 mod utils;
 
 use dag::DAG;
+use libp2p::identity;
 
-fn main() {
-    let mut dag = DAG::new();
+fn main(){
+    //generate keypair
+    let local_keys = identity::Keypair::generate_ed25519();
 
-    let id1 = dag.add_transaction("Transaction 1".to_string(), vec![]);
-    let id2 = dag.add_transaction("Transaction 2".to_string(), vec![id1.clone()]);
-    let id3 = dag.add_transaction("Transaction 3".to_string(), vec![id1.clone(), id2.clone()]);
+    //create peer id
+    let local_peer_id = libp2p::PeerId::from(local_keys.public());
 
-    for id in dag.transactions.keys().cloned().collect::<Vec<_>>() {
-        dag.calculate_cumulative_weight(&id);
-    }
-
-    println!("Transactions dans le DAG :");
-    dag.display();
+    println!("Local peer id: {:?}", local_peer_id);
 }
+
+
+
+
+// fn main() {
+//     let mut dag = DAG::new();
+
+//     let id1 = dag.add_transaction("Transaction 1".to_string(), vec![]);
+//     let id2 = dag.add_transaction("Transaction 2".to_string(), vec![id1.clone()]);
+//     let id3 = dag.add_transaction("Transaction 3".to_string(), vec![id1.clone(), id2.clone()]);
+
+//     for id in dag.transactions.keys().cloned().collect::<Vec<_>>() {
+//         dag.calculate_cumulative_weight(&id);
+//     }
+
+//     println!("Transactions dans le DAG :");
+//     dag.display();
+// }
