33c147e9179670e469bfaf24bfb4316c58bbe5bc
Winrhy <malo@arkada.fr>
2024-11-25 01:45:36 +0100

    feat: :triangular_flag_on_post: add simple method for validate transaction

--- src/main.rs
+++ src/main.rs
@@ -4,61 +4,57 @@ use std::{collections::HashMap, hash::Hash, vec};
 struct Transaction {
     id: String,
     data: String,
-    approvers: Vec<String>,
+    approves: Vec<String>,
 }
 
 #[derive(Debug)]
 struct DAG {
-    transations : HashMap<String, Transaction>,
+    transactions : HashMap<String, Transaction>,
 }
 
 impl DAG {
     //create new empty DAG
     fn new() -> DAG {
         DAG {
-            transations: HashMap::new(),
+            transactions: HashMap::new(),
         }
     }
 
     //add a transaction to the DAG
-    fn add_transaction(&mut self, id: String, data: String, approvers: Vec<String>) {
+    fn add_transaction(&mut self, id: String, data: String, approves: Vec<String>) {
         let transaction = Transaction {
             id: id.clone(),
             data: data,
-            approvers,
+            approves,
         };
-        self.transations.insert(id, transaction);
+        self.transactions.insert(id, transaction);
     }
 
     //get a transaction from the DAG
     fn display(&self) {
-        for (id, transaction) in &self.transations {
-            println!("Transaction id: {}, data: {}, Approvers {}", id, transaction.data, transaction.approvers.join(", "));
+        for (id, transaction) in &self.transactions {
+            println!("Transaction id: {}, data: {}, approves {}", id, transaction.data, transaction.approves.join(", "));
         }
     }
 
     //validate the DAG
-    fn validate_transaction(&self, id: String) -> bool {
-        let transaction = self.transations.get(&id);
-        match transaction {
-            Some(transaction) => {
-                for approver in &transaction.approvers {
-                    if !self.transations.contains_key(approver) {
-                        return false;
-                    }
+    fn validate_transaction(&self, id: &String) -> bool {
+        if let Some(transaction) = self.transactions.get(id) {
+            for approved_id in &transaction.approves {
+                if !self.transactions.contains_key(approved_id) {
+                    return false;
                 }
-                return true;
             }
-            None => return false,
+            true 
+        } else {
+            false 
         }
     }
 }
 
 fn main() {
-    // Créer un DAG
     let mut dag = DAG::new();
 
-    // Ajouter des transactions avec des liens
     dag.add_transaction("T1".to_string(), "Transaction 1".to_string(), vec![]);
     dag.add_transaction(
         "T2".to_string(),
@@ -71,7 +67,6 @@ fn main() {
         vec!["T1".to_string(), "T2".to_string()],
     );
 
-    // Valider des transactions
     println!("T3 est valide ? {}", dag.validate_transaction(&"T3".to_string())); // true
     println!("T4 est valide ? {}", dag.validate_transaction(&"T4".to_string())); // false
 }
