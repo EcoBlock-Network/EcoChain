b00bca07cf90ca80a42da88286b7f4936b81461a
Winrhy <malo@arkada.fr>
2024-11-25 01:34:24 +0100

    feat: :sparkles: add impl DAG

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
+use std::{collections::HashMap, hash::Hash};
+
 #[derive(Debug)]
 struct Transaction {
     id: String,
     data: String,
+}
+
+#[derive(Debug)]
+struct DAG {
+    transations : HashMap<String, Transaction>,
+}
+
+impl DAG {
+    //create new empty DAG
+    fn new() -> DAG {
+        DAG {
+            transations: HashMap::new(),
+        }
+    }
+
+    
 }
 
 fn main() {
