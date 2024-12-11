efc93028a0517da9fc7907498177ea254bca710c
Winrhy <malo@arkada.fr>
2024-12-11 19:47:48 +0100

    feat: :sparkles: Dissociate server and client

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{env, error::Error};
 use std::time::Duration;
 use futures::StreamExt;
 use libp2p::{
@@ -63,19 +63,31 @@ impl From<mdns::Event> for MyBehaviourEvent {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let is_server = args.iter().any(|arg| arg == "--server");
+    let is_client = args.iter().any(|arg| arg == "--client");
 
-    // INITIALIZE TRACING
+    println!(
+        "Mode d'exécution : {}",
+        if is_server {
+            "Serveur"
+        } else if is_client {
+            "Client"
+        } else {
+            "Client et Serveur (par défaut)"
+        }
+    );
+
+    // INIT
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .try_init()
         .unwrap();
 
-
-    // GENERATE LOCAL KEYPAIR AND PEER ID
+    // GENERATE PEER ID
     let local_key = libp2p::identity::Keypair::generate_ed25519();
     let local_peer_id = libp2p::PeerId::from(local_key.public());
     println!("Local Peer ID: {}", local_peer_id);
-
 
     // BUILD THE SWARM
     let mut swarm = libp2p::SwarmBuilder::with_new_identity()
@@ -90,25 +102,35 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 "discovery-example/1.0.0".to_string(),
                 local_key.public(),
             )),
-            rendezvous_client: client::Behaviour::new(local_key.clone()),
-            rendezvous_server: server::Behaviour::new(server::Config::default()),
+            rendezvous_client: if is_client || (!is_client && !is_server) {
+                client::Behaviour::new(local_key.clone())
+            } else {
+                client::Behaviour::new(local_key.clone())
+            },
+            rendezvous_server: if is_server || (!is_client && !is_server) {
+                server::Behaviour::new(server::Config::default())
+            } else {
+                server::Behaviour::new(server::Config::default())
+            },
             ping: ping::Behaviour::new(ping::Config::new().with_interval(Duration::from_secs(1))),
             mdns: mdns::async_io::Behaviour::new(mdns::Config::default(), local_peer_id).unwrap(),
         })?
         .build();
 
+    // LISTEN ON ALL INTERFACES (Serveur uniquement)
+    if is_server || (!is_client && !is_server) {
+        swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+        println!("Swarm listening on /ip4/0.0.0.0/tcp/0");
+    }
 
-    // LISTEN ON ALL INTERFACES
-    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
-    println!("Swarm listening on /ip4/0.0.0.0/tcp/0");
+    // REGISTER IN RENDEZVOUS SERVER (Client uniquement)
+    if is_client || (!is_client && !is_server) {
+        let namespace = Namespace::new("example-namespace".to_string()).expect("Invalid namespace");
+        let _ = swarm.behaviour_mut().rendezvous_client.register(namespace.clone(), local_peer_id, Some(3600));
+        println!("Registered in namespace: {}", namespace);
+    }
 
-
-    // REGISTER IN RENDEZVOUS SERVER
-    let namespace = Namespace::new("example-namespace".to_string()).expect("Invalid namespace");
-    let _ = swarm.behaviour_mut().rendezvous_client.register(namespace.clone(), local_peer_id, Some(3600));
-    println!("Registered in namespace: {}", namespace);
     let mut connected_peers: Vec<libp2p::PeerId> = Vec::new();
-
 
     // SWARM EVENT LOOP
     loop {
@@ -150,7 +172,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     }
                 }
             }
-            
             SwarmEvent::Behaviour(MyBehaviourEvent::Ping(event)) => {
                 println!("Ping event: {event:?}");
             }
